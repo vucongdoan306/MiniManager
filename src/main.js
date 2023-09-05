eb6c6e44202e14d007be8449a64f2e5e2b79a5e6
John Doe <noadvcxxi@gmail.com>
2023-09-05 14:49:30 +0700

    first

---
+++ src/main.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/order */
+import '@/@iconify/icons-bundle'
+import App from '@/App.vue'
+import vuetify from '@/plugins/vuetify'
+import { loadFonts } from '@/plugins/webfontloader'
+import router from '@/router'
+import '@core/scss/template/index.scss'
+import '@layouts/styles/index.scss'
+import '@styles/styles.scss'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+
+loadFonts()
+
+
+// Create vue app
+const app = createApp(App)
+
+
+// Use plugins
+app.use(vuetify)
+app.use(ElementPlus)
+app.use(createPinia())
+app.use(router)
+
+// Mount vue app
+app.mount('#app')
